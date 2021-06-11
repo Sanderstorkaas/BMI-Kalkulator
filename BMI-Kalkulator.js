8aec9baf8dbe59a773bacf1e16627bcc1fd51f8d
Sanderstorkaas <84008840+Sanderstorkaas@users.noreply.github.com>
2021-06-12 00:48:46 +0200

    Update BMI-Kalkulator.js

--- BMI-Kalkulator.js
+++ BMI-Kalkulator.js
@@ -1,59 +1,55 @@
 function measureBMI () {
-    let unit = document.getElementById("bmi-metric").checked,
-        weight = document.getElementById("bmi-weight"),
-        weightu = document.getElementById("bmi-weight-unit"),
-        height = document.getElementById("bmi-height"),
-        heightu = document.getElementById("bmi-height-unit");
+  let unit = document.getElementById("bmi-metric").checked,
+      weight = document.getElementById("bmi-weight"),
+      weightu = document.getElementById("bmi-weight-unit"),
+      height = document.getElementById("bmi-height"),
+      heightu = document.getElementById("bmi-height-unit");
     
-    if (unit) {
-      weightu.innerHTML = "KG";
-      weight.min = 1;
-      weight.max = 635;
-      heightu.innerHTML = "CM";
-      height.min = 54;
-      height.max = 272;
-    } else {
-      weightu.innerHTML = "LBS";
-      weight.min = 2;
-      weight.max = 1400;
-      heightu.innerHTML = "IN";
-      height.min = 21;
-      height.max = 107;
-    }
+  if (unit) {
+    weightu.innerHTML = "KG";
+    weight.min = 1;
+    weight.max = 635;
+    heightu.innerHTML = "CM";
+    height.min = 54;
+    height.max = 272;
+  } else {
+    weightu.innerHTML = "LBS";
+    weight.min = 2;
+    weight.max = 1400;
+    heightu.innerHTML = "IN";
+    height.min = 21;
+    height.max = 107;
   }
-  
-  function calcBMI () {
-   
-    let bmi = null,
-        unit = document.getElementById("bmi-metric").checked, 
-        weight = parseInt(document.getElementById("bmi-weight").value),
-        height = parseInt(document.getElementById("bmi-height").value),
-        results = document.getElementById("bmi-results");
-  
+}
+
+function calcBMI () {
+  let bmi = null,
+      unit = document.getElementById("bmi-metric").checked, 
+      weight = parseInt(document.getElementById("bmi-weight").value),
+      height = parseInt(document.getElementById("bmi-height").value),
+      results = document.getElementById("bmi-results");
     
-    if (unit) {
-      height = height / 100;
-      bmi = weight / (height * height);
-    }
-     
-    else {
-      bmi = 703 * (weight / (height * height));
-    }
-    
-    bmi = Math.round(bmi * 100) / 100; 
-  
-    if (bmi < 18.5) {
-      results.innerHTML = bmi + " - Undervektig";
-    } else if (bmi < 25) {
-      results.innerHTML = bmi + " - Normal vekt";
-    } else if (bmi < 30) {
-      results.innerHTML = bmi + " - Nesten-overvekt";
-    } else if (bmi < 35) {
-      results.innerHTML = bmi + " - Overvekt Klasse I";
-    } else if (bmi < 40) {
-      results.innerHTML = bmi + " - Overvekt Klasse II";
-    } else {
-      results.innerHTML = bmi + " - Overvekt Klasse III";
-    }
-    return false;
+  if (unit) {
+    height = height / 100;
+    bmi = weight / (height * height);
   }
+  else {
+    bmi = 703 * (weight / (height * height));
+  }
+  bmi = Math.round(bmi * 100) / 100; 
+  
+  if (bmi < 18.5) {
+    results.innerHTML = bmi + " - Undervektig";
+  } else if (bmi < 25) {
+    results.innerHTML = bmi + " - Normal vekt";
+  } else if (bmi < 30) {
+    results.innerHTML = bmi + " - Nesten-overvekt";
+  } else if (bmi < 35) {
+    results.innerHTML = bmi + " - Overvekt Klasse I";
+  } else if (bmi < 40) {
+    results.innerHTML = bmi + " - Overvekt Klasse II";
+  } else {
+    results.innerHTML = bmi + " - Overvekt Klasse III";
+  }
+  return false;
+}
